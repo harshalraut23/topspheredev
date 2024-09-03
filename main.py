1386d8aec7263ca81c0a045e6ddadb58af680002
harshal-raut-maersk <145019157+harshalraut23@users.noreply.github.com>
2024-09-03 11:46:14 +0530

    bug fix

--- main.py
+++ main.py
@@ -17,33 +17,37 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 # Function to connect to Azure SQL Database and fetch data
-def get_data_from_azure_sql(query):
-    server = 'top-insights-dev.database.windows.net'
-    database = 'top-insights-dev-db1'
-    username = 'CloudSA8bd104de'
-    password = 'KQxaecGh7K6V'
+# def get_data_from_azure_sql(query):
+#     server = 'top-insights-dev.database.windows.net'
+#     database = 'top-insights-dev-db1'
+#     username = 'CloudSA8bd104de'
+#     password = 'KQxaecGh7K6V'
     
-    connection_string = (
-        f"mssql+pyodbc://{username}:{password}@{server}/{database}"
-        f"?driver=ODBC+Driver+17+for+SQL+Server"
-    )
+#     connection_string = (
+#         f"mssql+pyodbc://{username}:{password}@{server}/{database}"
+#         f"?driver=ODBC+Driver+17+for+SQL+Server"
+#     )
     
-    engine = create_engine(connection_string)
-    df = pd.read_sql(query, engine)
-    return df
+#     engine = create_engine(connection_string)
+#     df = pd.read_sql(query, engine)
+#     return df
 
-# Fetch reports data from Azure SQL
-query_reports = "SELECT * FROM snow.techops_reports"
-df_reports = get_data_from_azure_sql(query_reports)
+# # Fetch reports data from Azure SQL
+# query_reports = "SELECT * FROM snow.techops_reports"
+# df_reports = get_data_from_azure_sql(query_reports)
+
+# @app.get("/", response_class=HTMLResponse)
+# async def read_root(request: Request):
+#     # Generate the HTML content
+#     # with open("templates/index.html") as file:
+#     #     template = Template(file.read())
+#     # reports_links = '\n'.join(f'<li><a href="/reports/{row["id"]}">{row["report_name"]}</a></li>' for index, row in df_reports.iterrows())
+#     # html_content = template.render(title="techOps sphere", reports_links=reports_links)
+#     return HTMLResponse(content=html_content)
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    # Generate the HTML content
-    with open("templates/index.html") as file:
-        template = Template(file.read())
-    reports_links = '\n'.join(f'<li><a href="/reports/{row["id"]}">{row["report_name"]}</a></li>' for index, row in df_reports.iterrows())
-    html_content = template.render(title="techOps sphere", reports_links=reports_links)
-    return HTMLResponse(content=html_content)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "title": "Home"})
 
 @app.get("/reports/{report_id}", response_class=HTMLResponse)
 async def read_report(report_id: int):
