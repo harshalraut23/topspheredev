155fe678542cdac3bcb1b731463693322e578f21
harshal-raut-maersk <145019157+harshalraut23@users.noreply.github.com>
2024-09-03 19:33:16 +0530

    changed the template

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
