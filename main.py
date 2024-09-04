a8ecec82559f671229299343a061725904dfee06
harshal-raut-maersk <145019157+harshalraut23@users.noreply.github.com>
2024-09-04 15:12:27 +0530

    revert the db scripts

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 # main.py
-from fastapi import FastAPI
-import pyodbc
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-def test_db_connection():
-    server = 'top-insights-dev.database.windows.net'
-    database = 'top-insights-dev-db1'
-    username = 'CloudSA8bd104de'
-    password = 'KQxaecGh7K6V'
-    driver = '{ODBC Driver 17 for SQL Server}'
-    
-    try:
-        with pyodbc.connect(
-            f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password};Encrypt=yes'
-        ) as conn:
-            return True
-    except Exception as e:
-        return str(e)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/test-db-connection")
-async def test_db():
-    result = test_db_connection()
-    if result is True:
-        return {"status": "Connection successful"}
-    else:
-        return {"status": "Connection failed", "error": result}
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
