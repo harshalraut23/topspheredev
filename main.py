d726003ddcc8edfb0d9dfed942121cf74dbcfdc8
harshal-raut-maersk <145019157+harshalraut23@users.noreply.github.com>
2024-09-04 14:39:08 +0530

    test db connection

--- main.py
+++ main.py
@@ -1,50 +1,28 @@
 # main.py
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from sqlalchemy import create_engine
-import pandas as pd
+from fastapi import FastAPI
 import pyodbc
 
-# Function to connect to Azure SQL Database and fetch data
-def get_data_from_azure_sql(query):
+app = FastAPI()
+
+def test_db_connection():
     server = 'top-insights-dev.database.windows.net'
     database = 'top-insights-dev-db1'
     username = 'CloudSA8bd104de'
     password = 'KQxaecGh7K6V'
+    driver = '{ODBC Driver 17 for SQL Server}'
     
-    connection_string = (
-        f"mssql+pyodbc://{username}:{password}@{server}/{database}"
-        f"?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=yes"        
-    )
-    
-    engine = create_engine(connection_string)
-    df = pd.read_sql(query, engine)
-    return df
+    try:
+        with pyodbc.connect(
+            f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password};Encrypt=yes'
+        ) as conn:
+            return True
+    except Exception as e:
+        return str(e)
 
-app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    # Fetch reports data from Azure SQL
-    query_reports = "SELECT * FROM snow.techops_reports WHERE section <> 'Success Stories'"
-    df_reports = get_data_from_azure_sql(query_reports)
-    
-    # Process the data into sections and reports
-    sections = {}
-    for _, row in df_reports.iterrows():
-        section = row['section']
-        if section not in sections:
-            sections[section] = []
-        sections[section].append({
-            "title": row['title'],
-            "weburl": row['weburl']
-        })
-    
-    # Pass sections to the template
-    return templates.TemplateResponse("index.html", {"request": request, "sections": sections})
+@app.get("/test-db-connection")
+async def test_db():
+    result = test_db_connection()
+    if result is True:
+        return {"status": "Connection successful"}
+    else:
+        return {"status": "Connection failed", "error": result}
