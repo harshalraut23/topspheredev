0b8093897d0688028707023af2ca88994f22a4e6
harshal-raut-maersk <145019157+harshalraut23@users.noreply.github.com>
2024-09-07 03:21:02 +0530

    latest changes to be commited

--- main.py
+++ main.py
@@ -18,3 +18,13 @@ async def read_root(request: Request):
 @app.get("/report1", response_class=HTMLResponse)
 async def report1(request: Request):
     return templates.TemplateResponse("report1.html", {"request": request})
+
+# Report 2 page
+@app.get("/report2", response_class=HTMLResponse)
+async def report1(request: Request):
+    return templates.TemplateResponse("report2.html", {"request": request})
+
+# Report 3 page
+@app.get("/report3", response_class=HTMLResponse)
+async def report1(request: Request):
+    return templates.TemplateResponse("report3.html", {"request": request})
